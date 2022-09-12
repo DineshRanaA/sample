a1f126287c539f08b1d4763e3519ac3aa65ccef4
Dinesh <dinesh@MacBook-Air-3.local>
2022-09-12 13:10:20 +0530

    socket check

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const express = require("express");
+const app = require("express")();
 const cors = require("cors");
-
-const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 app.use(cors("*"));
+
+io.on("connection", async (socket) =>  {
+    console.log(`a user has connected ${socket.id}`);
+
+    socket.on("_userAway", (payloadString) => {
+      console.log("_userAway",payloadString);
+    });
+  
+    socket.on("disconnect", () => {
+        console.log("disconnect",payloadString);
+    });
+})
 
 app.get('/',(req,res) => {
     res.json({
@@ -10,7 +22,7 @@ app.get('/',(req,res) => {
     });
 });
 
-const PORT = 3000;
+const PORT = 3002;
 app.listen(PORT, () => {
     console.log(`Server run in ${PORT}`);
 });
