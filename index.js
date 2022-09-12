c03fc52c3b7ae931d3bf6f735544f034be34d06d
Dinesh <dinesh@MacBook-Air-3.local>
2022-09-12 11:31:23 +0530

    test ip

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const cors = require("cors");
 const app = express();
 app.use(cors("*"));
 
+app.get('/',(req,res) => {
+    res.json({
+        message : 'testing'
+    });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server run in ${PORT}`);
