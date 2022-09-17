2e8866ebd1012a35e13885255495344a2d5047d2
Dinesh <dinesh@MacBook-Air-3.local>
2022-09-17 10:04:38 +0530

    socket test

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var app = express();
 
+require('dotenv').config();
+
 app.use(express.static(__dirname));
 
-const PORT = 3002;
+const PORT = process.env.PORT ?? 3002;
 const server = app.listen(PORT, () => {
     console.log(`Server run in ${PORT}`);
 });
 
 const io = require("socket.io")(server);
+
 io.on("connection", async (socket) =>  {
     console.log(`a user has connected ${socket.id}`);
 
