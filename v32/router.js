b3fcf50083165d3cf7d4aab4bb0c71c08123a2f7
Dinesh <dinesh@MacBook-Air-3.local>
2022-09-21 20:18:52 +0530

    Updated gitignore

--- v32/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// ROUTES
-const rewardRoutes = require("./routes/chatTheme.route");
-
-router.use("/chatTheme", rewardRoutes);
-
-module.exports = router;
