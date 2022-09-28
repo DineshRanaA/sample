559a12139ce40fffa342986b3c59d8ff25facc75
Dinesh <dinesh@MacBook-Air-4.local>
2022-09-28 18:02:11 +0530

    change post

--- v32/routes/chat.route.js
+++ v32/routes/chat.route.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const chatController = require("../controllers/chat.controller");
 const {validateUser} = require("../utils/helper.utils");
 
-router.get("/getChatId", validateUser, chatController.getChatId);
+router.post("/getChatId", validateUser, chatController.getChatId);
 
 module.exports = router;
