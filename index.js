6c8650e1cb7535928ad8c6b078aeb28d758baaed
Dinesh <dinesh@MacBook-Air-4.local>
2022-09-28 19:51:53 +0530

    check post

--- index.js
+++ index.js
@@ -16,6 +16,35 @@ app.get('/',(req,res) => {
         message : 'testing'
     });
 });
+app.post("/v2", (req, res) => {
+    res.json({
+      message: "success",
+    });
+});
 const server = app.listen(PORT, () => {
     console.log(`Server run in ${PORT}`);
+});
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", async (socket) =>  {
+    console.log(`a user has connected ${socket.id}`);
+    socket.on("_sendUserId", (payload) => {
+        var data = JSON.parse(payload);
+        socket.join("#"+data.userId);
+    });
+
+    socket.on("_sendMessage", (payload) => {
+        var data = JSON.parse(payload);
+        io.sockets.in("#"+data.receiverId).emit('_receiveMessage', payload);
+    });
+
+    socket.on("_typing", (payload) => {
+        var data = JSON.parse(payload);
+        io.sockets.in("#"+data.receiverId).emit('_typingStatus', payload);
+    });
 });
